64cb54e64641271e2fbcc9b01e66f5b06c4febd2
jnuno980 <jnuno980@gmail.com>
2025-10-15 09:46:19 +0100

    Trabalho local em Navbar.js

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Menu, X, BookOpen, Users, Library, FileText, Building, User, Tag, MapPin } from 'lucide-react';
+import { Menu, X, BookOpen, Building, User, Tag, MapPin } from 'lucide-react';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
